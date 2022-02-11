0796f6b8751158f7d3c0c0c6298090afc8639072
unknown <jonathannzete@gmail.com>
2022-02-11 15:23:22 +0100

    my_details url path moved up to avoid bug on url not found

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,10 +5,10 @@ from . import views
 
 urlpatterns = [
     path('', views.UserList.as_view(), name='users_list'),
-    path('<str:username>/', views.UserDetail.as_view(), name='users_profile'),
-    path('<uuid:pk>/follow/', views.FollowUser.as_view(), name='follow'),
     path('my_details/', views.LoggedInUser.as_view(), name='my_details'),
     path('my_details/update/', views.LoggedInUser.as_view(), name='update'),
+    path('<str:username>/', views.UserDetail.as_view(), name='users_profile'),
+    path('<uuid:pk>/follow/', views.FollowUser.as_view(), name='follow'),
     path('register/', views.UserRegister.as_view(), name='register'),
     path('activate_user/<uidb64>/<token>/', views.UserActivation.as_view(), name='activate_user'),
     path('password_reset/', views.PasswordReset.as_view(), name='password_reset'),
